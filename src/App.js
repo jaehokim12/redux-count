7895251e1eef46650c0dce3901b3ae0a92c151aa
jaehokim12 <62605981+jaehokim12@users.noreply.github.com>
2021-07-27 03:43:40 +0900

    decrease 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import createStore from './store';
 import reducers from './reducers';
-import { Provider } from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
 import React from 'react';
-import CountComponent from './components';
+import CountComponent from './components/count';
 const store = createStore(reducers);
 
 class App extends React.Component {
--- src/components/count.jsx
+++ src/components/count.jsx
@@ -1,19 +1,72 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import * as actions from '../action/count';
 
 class count extends React.Component {
-constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
+      localCount: 0,
     };
+  }
+  increaseCount = () => {
+    // same
+    this.setState((prevState) => {
+      return {
+        localCount: prevState.localCount + 1,
+      };
+    });
+  };
+  decreaseCount = () => {
+    // same
+    this.setState((prevState) => {
+      return {
+        localCount: prevState.localCount - 1,
+      };
+    });
+  };
+
+  render() {
+    // this:count {props: {…}, context: {…}, refs: {…}, updater: {…}, increaseCount: ƒ, …}
+    //this.props: {storeCount: 0, increaseStoreCount: ƒ, decreaseStoreCount: ƒ}
+    const { localCount } = this.state;
+    const { storeCount, increaseStoreCount, decreaseStoreCount } = this.props;
+    return (
+      <div>
+        {localCount}
+        <div
+          onClick={() => {
+            this.increaseCount(); // localCount+1
+            increaseStoreCount(); // action type increaseCount인 reducer실행 initialstate의 count +1
+          }}
+        >
+          {'+'}
+        </div>
+        <div
+          onClick={() => {
+            this.decreaseCount();
+            decreaseStoreCount();
+          }}
+        >
+          {'-'}
+        </div>
+      </div>
+    );
+  }
 }
-    
-    render() {
-        return <div></div>;
-    }
-}
-
-
-
-export default count;
+const mapStateToProps = (state) => ({
+  //counter.props:{storeCount:value}  접근가능
+  storeCount: state.count.count,
+});
+// countreducer를받아서 component의  state: countreducer의 initialstate의 value
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increaseStoreCount: () => dispatch(actions.increaseCount()),
+    decreaseStoreCount: () => dispatch(actions.decreaseCount()),
+  };
+};
+// dispatch 인자로받고
+export default connect(mapStateToProps, mapDispatchToProps)(count);
+//redux와count component를 합쳐 새로운 컴포넌트로 반환 countcomponent가 store에 접근할수있게하고 count로 리턴
